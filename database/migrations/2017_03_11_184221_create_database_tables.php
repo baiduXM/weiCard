06cca071b30f369d54f1b401eb84d5808176aed6
Hsieh <1195015834@qq.com>
2017-03-11 22:11:14 +0800

    updated 添加依赖，migration

--- database/migrations/2017_03_11_184221_create_database_tables.php
+++ database/migrations/2017_03_11_184221_create_database_tables.php
@@ -22,39 +22,53 @@ class InitDatabaseTables extends Migration
         // managers管理员表
         Schema::create('managers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
+            $table->string('name')->unique()->comment('账号');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
-            $table->string('mobile')->unique();
-            $table->string('nickname');
-            $table->tinyInteger('is_super')->unsigned();
-            $table->tinyInteger('is_active')->unsigned();
+            $table->string('mobile')->unique()->nullable();
+            $table->string('nickname')->nullable();
+            $table->tinyInteger('is_super')->unsigned()->default(0);
+            $table->tinyInteger('is_active')->unsigned()->default(1);
             $table->timestamps();
+            $table->softDeletes();
+            $table->comment = '管理员表';
         });
 
         // contacts通讯录表
-        Schema::create('members', function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table) {
             $table->increments('id');
+            $talbe->integer('user_id');
+            $talbe->integer('follow_id');
+            $talbe->tinyInteger('type');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nickname');
+            $table->string('telephone');
+            $table->string('mobile');
+            $table->string('group');
+            $table->string('company');
+            $table->string('address');
+            $table->string('description');
             $table->timestamps();
         });
 
         // companies公司表
-        Schema::create('members', function (Blueprint $table) {
+        Schema::create('companies', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('logo');
+            $table->string('address');
+            $table->string('telephone');
+            $table->string('description');
+            $table->string('code');
+            $table->tinyInteger('status');            
             $table->timestamps();
+            $table->softDeletes();
         });
 
         // employees员工表
-        Schema::create('members', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
