018c34b86912c41dc45c559f05b7905d73b78d64
Hsieh <1195015834@qq.com>
2017-05-11 11:30:29 +0800

    Revert "updated oauth debug"
    This reverts commit 957c765ea94fb2f2f1fc15a16e28784829035d07.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -117,7 +117,7 @@ class AuthController extends Controller
     }
 
     /**
-     * 第三方登录 - 请求接口
+     * 第三方登录请求接口
      *
      * @param Request $request
      * @return mixed
@@ -128,13 +128,13 @@ class AuthController extends Controller
     }
 
     /**
-     * 第三方登录 - 回调地址
+     * 第三方登录回调地址
      *
      * @param Request $request
      */
     public function handleProviderCallback(Request $request, $driver)
     {
-        $oauthUser = Socialite::with($driver)->user();
+        $oauthUser = \Socialite::with($driver)->user();
         dd($oauthUser);
     }
 
