9001f3f9552fa8053b9da4f45230cd5356aa46de
pdc <pengdingchao@xm12t.com>
2017-05-18 17:29:09 +0800

    微信接口js-api接入（测试2）

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -109,7 +109,7 @@ class IndexController extends Controller
         $wx['jsapi_ticket'] = $this->wx_get_jsapi_ticket();
         //分享的地址，注意：这里是指当前网页的URL，不包含#及其后面部分
         $wx['url'] = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-        $string = sprintf("jsapi_ticket=%s&noncestr=%s×tamp=%s&url=%s", $wx['jsapi_ticket'], $wx['noncestr'], $wx['timestamp'], $wx['url']);
+        $string = sprintf("jsapi_ticket=%s&noncestr=%s&timestamp=%s&url=%s", $wx['jsapi_ticket'], $wx['noncestr'], $wx['timestamp'], $wx['url']);
         //生成签名
         $wx['signature'] = sha1($string);
         return $wx;
