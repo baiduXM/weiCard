1bfabb21211a06a5770a8a2a30b9e29d1fd11a98
bdxm <gbpen@xm12t.com>
2017-05-23 16:33:10 +0800

    Please enter the commit message for your changes. Lines starting

--- public/templates/W0001PCN01/index.blade.php
+++ public/templates/W0001PCN01/index.blade.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <script type="text/javascript">
-  // 跳转PC页面
-  {if $site_another_url}
-      if (!navigator.userAgent.match(/AppleWebKit.*Mobile.*/)) {
-          location.href = '{$site_another_url}';
-      }
-  {/if}
-  </script>
   <meta name="x5-orientation" content="portrait">
   <title>G宝盆--名片</title>
   <meta name="keywords" content="G宝盆--名片" />
@@ -53,7 +45,7 @@
         <a href="" class="set animated fadeInRightBig go"></a>
       </div>
       <div class="picinfo">
-        <a href="" class="concern fl slower rotateInDownLeft go"></a>
+        <a href="{{'http://'.$server_name.'/cardcase/follow/e-'.$employee->id}}" class="concern fl slower rotateInDownLeft go"></a>
         <div class="portrait fl">
           <div class="div_img animated rotateIn go"><img src="{{'http://'.$server_name.'/'.$company->logo}}"></div>
           <h2 class="wow slowest flipInX go">{{$company->name}}</h2>
@@ -69,7 +61,7 @@
 
   <div class="information">
     <p class="p1 animated fadeInUp go">{{ $employee->title }}</p>
-    <p class="p2 slow fadeInUp go">{{ $employee->mobile }}</p>
+    <a href="tel:{{ $employee->mobile }}"><p class="p2 slow fadeInUp go">{{ $employee->mobile }}</p></a>
     <p class="p3 slower fadeInUp go">{{$company->email}}</p>
     <p class="p4 slowest fadeInUp go">{{ $company->address }}</p>
   </div>
@@ -81,8 +73,8 @@
   </div>
 <div class="footer">
   <a href="javascript:;" class="tel same animated fadeInLeftBig go">一键拨号</a>
-  <a href="" class="name same slow fadeInLeftBig go">名片夹</a>
-  <a href="" class="follow same slower fadeInRightBig go">关注</a>
+  <a href="{{'http://'.$server_name.'/cardcase'}}" class="name same slow fadeInLeftBig go">名片夹</a>
+  <a href="{{'http://'.$server_name.'/cardcase/follow/e-'.$employee->id}}" class="follow same slower fadeInRightBig go">关注</a>
   <a href="" class="share same slowest fadeInRightBig go fxBtn">分享</a>
 </div>
 </div>
@@ -91,14 +83,24 @@
 <div class="call titlerer flip go">
   <div class="img1 slowest tada go"><img src="{{ asset('templates/'.$template_name.'/images/tel.png') }}"></div>
   <h2>手机</h2>
-  <p class="p">{{ $employee->mobile }}<em class="slower wobble go"></em></p>
+  <a href="tel:{{ $employee->mobile }}"><p class="p">{{ $employee->mobile }}<em class="slower wobble go"></em></p></a>
   <!-- <p class="p">18759975489<em class="slower wobble go"></em></p> -->
   <!-- <p class="p">18759975489<em class="slower wobble go"></em></p> -->
   <div class="close"><em class="slowest wobble go"></em></div>
 </div>
 <div class="call1 animated flip go">
-  <div class="img1"><img src="{{ $qrcodeurl }}"></div>
-  <h2>“扫一扫”我的名片二维码</h2>
+  <div class="wrap_code">
+    <div class="list">
+      <h2>长按二维码保存到手机通讯录</h2>
+      <div class="img1"><img src="{{ $mpqrcodeurl }}"></div>
+    </div>
+    <div class="card">
+      <h2>长按二维码收藏我的移动名片</h2>
+      <div class="img1"><img src="{{ $qrcodeurl }}"></div>
+    </div>
+  </div>
+  <div class="tab"><a href="javascript:;" class="li1 on"><em>通讯录</em></a><a href="javascript:;" class="ca1"><em>名片</em></a></div>
+  <h2>点击图标可切换对应的二维码</h2>
   <div class="img2 slowest tada go"><img src="{{ asset('templates/'.$template_name.'/images/c2.png') }}"></div>
   <div class="close"><em></em></div>
 </div>
@@ -108,3 +110,48 @@
   <span class="fxClose">[关闭]</span>
 </div>
 </html>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
+<script>
+    wx.config({
+        debug: false,
+        appId: '{{ $AppID }}',
+        timestamp: "{{ $timestamp }}",
+        nonceStr: '{{ $noncestr }}',
+        signature: '{{ $signature }}',
+        jsApiList: [
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage'
+        ]
+    });
+    wx.ready(function () {
+
+        wx.onMenuShareTimeline({
+            title: 'G宝盆-名片', // 分享标题
+            link: '{{'http://'.$server_name.'/cardview?com='.$employee->company_id}}&&emp={{$employee->id}}', // 分享链接
+            imgUrl: '{{'http://'.$server_name.'/'.$company->logo}}', // 分享图标
+            success: function () {
+                // 用户确认分享后执行的回调函数
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+
+        wx.onMenuShareAppMessage({
+            title: 'G宝盆-名片', // 分享标题
+            desc: '{{ $employee->name }}', // 分享描述
+            link: '{{'http://'.$server_name.'/cardview?com='.$employee->company_id}}&&emp={{$employee->id}}', // 分享链接
+            imgUrl: '{{'http://'.$server_name.'/'.$company->logo}}', // 分享图标
+            type: '', // 分享类型,music、video或link，不填默认为link
+            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () {
+                // 用户确认分享后执行的回调函数
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+
+    });
+
+</script>
--- public/templates/W0001PCN01/js/function.js
+++ public/templates/W0001PCN01/js/function.js
@@ -18,6 +18,21 @@ $(document).ready(function(){
         $(".fxBtn,.fxClose").bind('touchstart', function(event) {
         $("#fenxiang").slideToggle();
         return false;
-    });
+        });
+        (function(){
+            $('.tab a').each(function(index){
+                $(this).click(function(){
+                    $('.tab a').removeClass('on')
+                    $(this).addClass('on')
+                    if(index==0){
+                        $('.list').css('display','block')
+                        $('.card').css('display','none')
+                    } else{
+                        $('.list').css('display','none')
+                        $('.card').css('display','block')
+                    }
+                })
+            })
+        })();
   });
   
