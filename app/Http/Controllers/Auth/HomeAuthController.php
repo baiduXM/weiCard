46948daf669ddd5cc4d76624731f714527d3443c
Hsieh <1195015834@qq.com>
2017-07-06 11:45:07 +0800

    updated login

--- app/Http/Controllers/Auth/HomeAuthController.php
+++ app/Http/Controllers/Auth/HomeAuthController.php
@@ -187,13 +187,14 @@ class HomeAuthController extends HomeController
     {
         // 网页扫码没有关注公众号字段
         // 登录/注册
-        $user = User::where('oauth_weixin', '=', $data['unionid'])->first();
+        $user = User::orWhere('oauth_weixin', $data['unionid'])->orWhere('name', $data['openid'])->first();
+
         if ($user) { // 存在，登录
             return Auth::guard($this->getGuard())->login($user);
         } else { // 不存在，创建，登录
             // openid:当前公众号授权唯一码
             // unionid:同一个开发平台用户唯一码
-            $array['name'] = $data['unionid']; // 当前公众号唯一码
+            $array['name'] = $data['openid']; // 当前公众号唯一码
             $array['sex'] = $data['sex'];
             $array['avatar'] = $data['headimgurl']; // TODO：下载远程图片到本地
             $array['nickname'] = $data['nickname'];
