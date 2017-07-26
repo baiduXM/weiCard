52ab5a2557c034f9a22f45bf6c5c3cc54fa90ff5
Hsieh <1195015834@qq.com>
2017-07-26 17:06:47 +0800

    updated 添加员工状态

--- resources/views/web/employee/index.blade.php
+++ resources/views/web/employee/index.blade.php
@@ -61,6 +61,7 @@
                     <th class="b-phone-w2"><a href="">部门</a></th>
                     <th class="b-phone-w2"><a href="">职位</a></th>
                     <th class=" b-td-hide"><a href="">手机</a></th>
+                    <th class=" b-td-hide"><a href="">状态</a></th>
                     <th class=" b-td-show"><a href="javascript:"><i class="iconFont">&#xe652;</i></a></th><!--适应手机-->
                     <th class=" b-td-hide"><a href="">操作</a></th>
                 </tr>
@@ -81,6 +82,7 @@
                             <td class="">{{ $item->department ?$item->department->name:'' }}</td>
                             <td class="">{{ $item->positions or (isset($item->position->name)?$item->position->name:'') }}</td>
                             <td class="b-td-width b-td-hide">{{ $item->mobile }}</td>
+                            <td class="b-td-width b-td-hide">{{ $item->user_id ? '已绑定' : '' }}</td>
                             <td class="b-td-icon b-td-hide w-icon">
                                 <a href="" data-toggle="modal" data-target="#modal-employee-show"
                                    class="operate-show"
