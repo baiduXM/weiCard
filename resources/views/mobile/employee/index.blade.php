12c3c17a049c6c39edfb4031dfef4b67aeb7cbfd
pdc <pengdingchao@xm12t.com>
2017-08-17 17:02:57 +0800

    update

--- resources/views/mobile/employee/index.blade.php
+++ resources/views/mobile/employee/index.blade.php
@@ -36,10 +36,10 @@
         <div class="card_content">
             @if($employee)
                 <div class="cards pr"><img src="{{ asset('static/mobile/images/bg9.png') }}">
-                    <div class="cards_tx dtc tc vm pa"><img
+                    <div class="cards_tx1 dtc tc vm pa"><img
                                 src="{{ $employee->avatar ? asset($employee->avatar) : asset('static/mobile/images/qy_tx.png') }}">
                     </div>
-                    <div class="qy_name pa">{{ $employee->company->display_name }}</br>
+                    <div class="qy_name1 pa">{{ $employee->company->display_name }}</br>
                         {{ $employee->nickname }}{{ $employee->positions ? '&nbsp;' . $employee->positions : ($employee->position ? '&nbsp;' . $employee->position->name : '') }}
                         
                     </div>
